2f5cdc8def8777a51d56d91b94e481b9322bfc60
ibraheemnadeem7 <157302504+ibraheemnadeem7@users.noreply.github.com>
2024-02-06 22:53:47 -0500

    Update SearchPrefix.rb

--- SearchPrefix.rb
+++ SearchPrefix.rb
@@ -1,7 +1,24 @@
 def searchPrefix(arr,s)
+    starting_char_length = s.length()
+    starting_char_length = starting_char_length - 1
+    newarr = []
+    for element in arr
+      if element[0..(starting_char_length)] != s        # puts "#{element} starts with #{s}"
 
+        # puts "#{element} does not starts with #{s}"
+      else
+        newarr.push(element)
+      end
+    end
+    puts "the elements in #{arr} that start with the character #{s} are #{newarr}"
 end
 
 print(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"ab"))
 
 print(searchPrefix(["abcde", "abdf", "adeab", "abdgse", "bdefa", "bacdef"],"b"))
+
+print(searchPrefix(["Ijcenjc", "cwec", "cwecwec", "cqecwwc", "bdefa", "bacdef"],"I"))
+
+print(searchPrefix(["lolHEY", "lolTHIS", "adeab", "abdgse", "bdefa", "lolME"],"lol"))
+
+print(searchPrefix(["2bill", "3sam", "1anjali", "1max", "5morgan"],"1"))
